48aefd91205d466d2fb9ea96c7108715bd0a1092
AllenB23 <46408750+Byen23@users.noreply.github.com>
2019-01-06 07:34:53 -0800

    2.Input-Print-Function-Program
    Second Python program uploaded to Github.
    A simple program that asks users name then outputs a simple greeting with the username.

---
+++ 2.Input-Print-Function.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  6 07:20:03 2019
+
+@author: Byen23
+"""
+
+#A simple input and print function program
+'''Ask user name, then prints out a simple greeting with 
+user name.'''
+
+name = input("Enter your name: ")
+
+print("Hello!",name,"\nMy name is Bob.",
+      "\nNice to meet you.")
